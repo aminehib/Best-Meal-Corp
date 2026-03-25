221a906b2fb33795d7b0ce385e4ed45f404b6477
soheybkorichi41 <soheybkorichi41@gmail.com>
2026-03-25 22:28:19 +0100

    Add files via upload

---
+++ projet-recette/js/login.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+   const adminBtn = document.getElementById("admin-btn");
+
+   adminBtn.textContent = "Logout";
+    adminBtn.href = "admin/logout.php";
+
+}
+)
