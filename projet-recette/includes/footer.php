3b855a8cecfdf557b6122e6f7eb3e2b90bc69d8c
soheybkorichi41 <soheybkorichi41@gmail.com>
2026-04-04 01:08:10 +0200

    Add files via upload

---
+++ projet-recette/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="site-footer">
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> - Livret de Recettes</p>
+        <p>Sources des images : Pexels, Unsplash.</p>
+        
+    </div>
+</footer>
