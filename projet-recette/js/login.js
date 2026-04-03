ed0ad000aba83775c541850252204b7fc5858769
soheybkorichi41 <soheybkorichi41@gmail.com>
2026-04-03 17:23:12 +0200

    Delete projet-recette directory

--- projet-recette/js/login.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-   const adminBtn = document.getElementById("admin-btn");
-
-   adminBtn.textContent = "Logout";
-    adminBtn.href = "admin/logout.php";
-
-}
-)
